94ebc6fcb86c3cd709147e974a0df2ddbcf93f48
hmcurt01 <haydencurtis77@gmail.com>
2018-12-03 14:00:18 -0500

    first commit
    character class, choose character function

---
+++ main.py
@@ -0,0 +1,19 @@
+#main file
+from pygame import *
+
+class Character:
+
+    def __init__(self, health, weapon, name, appearance, id):
+        self.health = health
+        self.weapon = weapon
+        self.name = name
+        self.appearance = appearance
+        self.id = id
+
+#choose player character
+def choose_character():
+    #choose character
+    player = Character(3, )
+
+
+        
